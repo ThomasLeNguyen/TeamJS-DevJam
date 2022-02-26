98249b3188bf3b6e0dbb07cc1d9e5c53a1d5bf27
thomas <thomaslenguyenasian@gmail.com>
2022-02-26 11:17:12 -0800

    11:17

--- index.js
+++ index.js
@@ -3,40 +3,26 @@ const context = canvas.getContext("2d");
 
 let keys = { w: false, a: false, s: false, d: false }
 
-addEventListener("keydown", function() {
-    if(!keys.w) keys.w = true;
-
-    if(keys.w) console.log("W key pressed!");
-
-    if(!keys.a) keys.a = true;
-
-    if(keys.a) console.log("A key pressed!");
-
-    if(!keys.s) keys.s = true;
-
-    if(keys.s) console.log("S key pressed!");
-
-    if(!keys.d) keys.d = true;
-
-    if(keys.d) console.log("D key pressed!");
+addEventListener("keydown", event => {
+    if(event.key === "w") keys.w = true;
+    if(event.key === "a") keys.a = true;
+    if(event.key === "s") keys.s = true;
+    if(event.key === "d") keys.d = true;
+    // if(keys.w) console.log("W key pressed!");
+    // if(keys.a) console.log("A key pressed!");
+    // if(keys.s) console.log("S key pressed!");
+    // if(keys.d) console.log("D key pressed!");
 });
 
-addEventListener("keyup", function() {
-    if(keys.w) keys.w = false;
-
-    if(!keys.w) console.log("W key unpressed!");
-
-    if(keys.a) keys.a = false;
-
-    if(!keys.a) console.log("A key unpressed!");
-
-    if(keys.s) keys.s = false;
-
-    if(!keys.s) console.log("S key unpressed!");
-
-    if(keys.d) keys.d = false;
-
-    if(!keys.d) console.log("D key unpressed!");
+addEventListener("keyup", event => {
+    if(event.key === "w") keys.w = false;
+    if(event.key === "a") keys.a = false;
+    if(event.key === "s") keys.s = false;
+    if(event.key === "d") keys.d = false;
+    // if(!keys.w) console.log("W key unpressed!");
+    // if(!keys.a) console.log("A key unpressed!");
+    // if(!keys.s) console.log("S key unpressed!");
+    // if(!keys.d) console.log("D key unpressed!");
 });
 
 
